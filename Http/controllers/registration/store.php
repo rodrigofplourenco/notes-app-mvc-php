ab59de886780516cc8e16d96b9e60a49c2f3a945
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2025-01-04 15:10:46 +0000

    Legend has born

---
+++ Http/controllers/registration/store.php
@@ -0,0 +1,43 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (! Validator::email($email)) {
+  $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (! Validator::string($password, 7, 100)) {
+  $errors['password'] = 'Please provide a password of at least seven characters.';
+}
+
+if (! empty($errors)) {
+  return view('registration/create.view.php', compact('errors'));
+}
+
+$user = $db->query('SELECT * FROM users WHERE email = :email', compact('email'))->find();
+
+if ($user) {
+  header('location: /');
+  exit();
+} else {
+  $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+    'email' => $email,
+    'password' => password_hash($password, PASSWORD_DEFAULT),
+  ]);
+
+  login([
+    'email' => $email
+  ]);
+
+  header('location: /');
+  exit();
+}
---
+++ Http/controllers/session/store.php
@@ -0,0 +1,24 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+
+$form = LoginForm::validate($attributes = [
+  'email' => $_POST['email'],
+  'password' => $_POST['password'],
+]);
+
+$signedIn = (new Authenticator)->attempt(
+  $attributes['email'],
+  $attributes['password']
+);
+
+if (!$signedIn) {
+  $form->error(
+    'password',
+    'No matching account found for that email address and password.'
+  )
+    ->throw();
+}
+
+redirect('/');
